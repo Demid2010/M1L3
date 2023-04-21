c6278983d3a193de1c1ba0fdcd46491f830f2ea4
Demid2010 <129614903+Demid2010@users.noreply.github.com>
2023-04-21 14:32:13 +0300

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,20 @@
+import random
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def flip_coin():
+    input("Орел или решка?:")
+    flip = random.randint(0, 1)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
